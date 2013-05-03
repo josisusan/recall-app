4e53b06f3803aede9ecdef933cfefbb7ce3d6186
josisusan <josisusan@gmail.com>
2013-05-03 23:10:33 +0545

    Initialize DataMapper with SQlite3 db. Define Note table

---
+++ recall.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'sinatra'
+require 'datamapper'
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/recall.db")
+
+class Note
+  include DataMapper::Resource
+  property :id, Serial
+  property :content, Text, :required => true
+  property :completed, Boolean, :required => true, :default => false
+  property :created_at, DateTime
+  property :updated_at, DateTime
+end
+
+DataMapper.finalize.auto_upgrade!
