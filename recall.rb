5dc6ffd20d3b972b6c235d386c4d36395f90c510
josisusan <josisusan@gmail.com>
2013-05-04 10:32:32 +0545

    Add basic functionality of edit delete and show page

--- recall.rb
+++ recall.rb
@@ -8,7 +8,7 @@ class Note
   include DataMapper::Resource
   property :id, Serial
   property :content, Text, :required => true
-  property :completed, Boolean, :required => true, :default => false
+  property :complete, Boolean, :required => true, :default => false
   property :created_at, DateTime
   property :updated_at, DateTime
 end
@@ -19,4 +19,48 @@ get '/' do
   @notes = Note.all :order => :id.desc
   @title = 'All Notes'
   erb :home
+end
+
+post '/' do
+  note = Note.new
+  note.content = params[:content]
+  note.created_at = Time.now
+  note.updated_at = Time.now
+  note.save
+  redirect '/'
+end
+
+get '/:id/edit' do
+  @note = Note.get params[:id]
+  @title = "Edit note"
+  erb :edit
+end
+
+put '/:id' do
+  note = Note.get params[:id]
+  note.content = params[:content]
+  note.complete = params[:complete] ? true : false
+  note.updated_at = Time.now
+  note.save
+  redirect '/'
+end
+
+get '/:id/delete' do
+  @note = Note.get params[:id]
+  @title = "Confirm Deletion"
+  erb :delete
+end
+
+delete '/:id' do
+  note = Note.get params[:id]
+  note.destroy
+  redirect '/'
+end
+
+get '/:id/complete' do
+  note = Note.get params[:id]
+  note.complete = note.complete ? 0 : 1
+  note.updated_at = Time.now
+  note.save
+  redirect '/'
 end
